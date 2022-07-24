4a4a9021e54c23cf7064b0f1742c51c1d2b52e47
iamausafhussain <drrespect2000@gmail.com>
2022-07-24 08:44:05 +0530

    coding

--- asad.py
+++ asad.py
@@ -1,14 +1,32 @@
-data = []
 
-print("Please tell me about yourself: ")
+# 4
+# red
+# blue
+# green
+# yellow
+# 1
+# yellow
 
-while True:
-    line = input()
-    if line:
-        data.append(line)
-    else:
-        break
-finalText = "\n".join(data)
+n = int(input())
+l = []
 
-print()
-print(finalText)
+for i in range(n):
+    a = input()
+    l.append(a)
+
+startIndex = int(input())
+target = input()
+
+print(l)
+l1 = l[startIndex::] + l[:startIndex:]
+l2 = l[startIndex:: -1] + l[:startIndex: -1]
+print(l1)
+print(l2)
+
+right = l1.index(target)
+left = l2.index(target)
+
+if right > left:
+    print(left)
+else:
+    print(right)
